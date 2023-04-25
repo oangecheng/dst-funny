50ed4ea81f5821d0040ab72def08b48fd62b10bc
zax <281725011@qq.com>
2023-04-26 01:58:42 +0800

    面板完善

--- modmain.lua
+++ modmain.lua
@@ -4,6 +4,11 @@ PrefabFiles = {
     "ksfun_tasks",
 }
 
+Assets = {
+    Asset("ATLAS", "images/ksfun_player_panel_bg.xml"),
+    Asset("IMAGE", "images/ksfun_player_panel_bg.tex"),
+}
+
 require("ksfun_tuning")
 
 
@@ -12,15 +17,31 @@ AddReplicableComponent("ksfun_hunger")
 modimport("scripts/strings_c.lua")
 
 
-local medalPage = require "mod/my_screen"
+local player_panel = require "mod/my_screen"
 AddClassPostConstruct("widgets/controls", function(self, owner)
-	self.medalPage = self:AddChild(medalPage(self.owner))--说明页图标
+	owner.player_panel = self:AddChild(player_panel(self.owner))--说明页图标
+    owner.player_panel:Hide()
+    owner.player_panel_showing = false
 end)
 
 
 -- 加载各种玩法
 modimport("scripts/mod/ksfun_policy_hunger.lua")
 modimport("scripts/mod/ksfun_clotheses.lua")
+
+
+AddPlayerPostInit(function(inst)
+    TheInput:AddKeyDownHandler(108, function() 
+        if inst.player_panel_showing then
+            inst.player_panel:Hide()
+            inst.player_panel_showing = false
+        else
+            inst.player_panel:Show()
+            inst.player_panel_showing = true
+        end
+
+    end)
+end)
 
 if GLOBAL.TheNet:GetIsServer() then
     modimport("scripts/mod/ksfun_policy_health.lua")
--- scripts/mod/my_screen.lua
+++ scripts/mod/my_screen.lua
@@ -1,24 +1,41 @@
 local Widget = require "widgets/widget"
 local TextButton = require "widgets/textbutton"
+local Button = require "widgets/button"
 local Text = require "widgets/text"
 local Image = require "widgets/image"
 
-local medalPage = Class(Widget, function(self, owner)
-	Widget._ctor(self, "medalPage")
-	self.root = self:AddChild(Widget("ROOT"))		
+local KSFUN_PLAYER_PANEL = Class(Widget, function(self, owner)
+	Widget._ctor(self, "KSFUN_PLAYER_PANEL")
+	self.root = self:AddChild(Widget("ROOT"))
+	
+	local scale = 2
+	local x = 0
+	local y = 500
+	local half_w = 512 * scale / 4
+	local half_h = 512 * scale / 4
+	
+	self.bg = self.root:AddChild(Image("images/ksfun_player_panel_bg.xml", "ksfun_player_panel_bg.tex"))
+	self.bg:SetScale(scale, scale, scale)
+	self.bg:SetHAnchor(0)
+	self.bg:SetVAnchor(0)
+	self.bg:SetPosition(0, y, 0)
+
+	
 	self.pageIcon = self.root:AddChild(TextButton())
-    self.pageIcon:SetText("OK")
+    self.pageIcon:SetText("关闭")
 	self.pageIcon:SetScale(2, 2, 2)
 	self.pageIcon:SetHAnchor(0) -- 设置原点x坐标位置，0、1、2分别对应屏幕中、左、右
 	self.pageIcon:SetVAnchor(0) -- 设置原点y坐标位置，0、1、2分别对应屏幕中、上、下
-	self.pageIcon:SetPosition(26,26,0)
-	self.pageIcon:SetTooltip("123")--tips
+	self.pageIcon:SetPosition(x,  y - half_h - 25, 0)
+
+
 	self.pageIcon:SetOnClick(function()
-		-- VisitURL("https://www.guanziheng.com/", false)
+		self:Hide()
+		owner.player_panel_showing = false
 		if owner.replica.ksfun_hunger then
 			self.pageIcon:SetText("level "..owner.replica.ksfun_hunger.level)
 		end
 	end)
 end)
 
-return medalPage
+return KSFUN_PLAYER_PANEL
