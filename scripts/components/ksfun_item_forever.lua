ee0cd5e3a0cbafecf8027fbc0ab07636f75b2696
chengzhi <chengzhi@kuaishou.com>
2023-05-29 16:13:56 +0800

    铠甲无法移除策略

--- scripts/components/ksfun_item_forever.lua
+++ scripts/components/ksfun_item_forever.lua
@@ -4,6 +4,14 @@ local itemsdef = {
 }
 
 
+local function isValidItem(self, item)
+    if item.prefab == itemsdef[1] then
+        return true
+    end
+    return false
+end
+
+
 local function setItemEnable(self, enable)
     local weapon = self.inst.components.weapon
     if weapon then
@@ -23,7 +31,7 @@ end
 
 local function traderTest(self, item, giver)
     if self.enable then
-        if item.prefab == itemsdef[1] then
+        if isValidItem(self, item) then
             return true
         end
     end
@@ -31,39 +39,49 @@ local function traderTest(self, item, giver)
 end
 
 
+
+
+
 local function onAccept(self, giver, item)
+    KsFunLog("onAccept", item.prefab, self.enable)
+
+    if not self.enable then return end
+    if not isValidItem(self, item) then return end
+
     local finiteuses = self.inst.components.finiteuses
-    KsFunLog("onAccept", item.prefab)
-    if self.enable and finiteuses then
-        if item.prefab == itemsdef[1] then
-            local percent = finiteuses:GetPercent() + 0.2
-            KsFunLog("onAccept", percent)
-            finiteuses:SetPercent(percent)
-            setItemEnable(self, true)
+    local armor = self.inst.components.armor
+
+
+    if finiteuses then
+        local percent = finiteuses:GetPercent() + 0.2
+        finiteuses:SetPercent(percent)
+        setItemEnable(self, true)
+    elseif armor then
+        local percent = armor:GetPercent() + 0.2
+        armor:SetPercent(percent)
+        setItemEnable(self, true)
+    end
+
+
+    if self.enable then
+        if self.inst.components.finiteuses then
+            local finiteuses = self.inst.components.finiteuses
+            if item.prefab == itemsdef[1] then
+                local percent = finiteuses:GetPercent() + 0.2
+                KsFunLog("onAccept", percent)
+                finiteuses:SetPercent(percent)
+                setItemEnable(self, true)
+            end
+        elseif self.inst.components.armor then
+
         end
+ 
     end
 end
 
 
-
-
-local FOREVER = Class(function(self, inst)
-    self.inst = inst
-    self.enable = false
-
-    self.data = {}
-
-    -- 武器数据缓存
-    if inst.components.weapon then
-        self.data.damage = inst.components.weapon.damage
-    end
- 
-
-    ---加上交易组件
-    if inst.components.trader == nil then
-        inst:AddComponent("trader")
-    end
-
+--- 初始化交易组件，和其他mod兼容
+local function initTrader(self)
     local trader = self.inst.components.trader
     local oldTradeTest = trader.abletoaccepttest
     trader:SetAbleToAcceptTest(function(inst, item, giver)
@@ -84,7 +102,33 @@ local FOREVER = Class(function(self, inst)
         end
         giver.SoundEmitter:PlaySound("dontstarve/common/telebase_gemplace")
     end
+end
 
+
+
+local FOREVER = Class(function(self, inst)
+    self.inst = inst
+    self.enable = false
+
+    self.data = {}
+
+    -- 武器数据缓存
+    if inst.components.weapon then
+        self.data.damage = inst.components.weapon.damage
+    end
+ 
+
+    ---加上交易组件
+    if inst.components.trader == nil then
+        inst:AddComponent("trader")
+    end
+    initTrader(self)
+
+
+    -- 监听装备时间，缓存owner
+    self.inst:ListenForEvent("equipped", function(inst, data)
+        self.inst.ksfunitemowner = data.owner
+    end)
     
 end)
 
@@ -92,12 +136,26 @@ end)
 function FOREVER:Enable()
     self.enable = true
 
-    --- 启用时，物品不会被移除
+    --- 启用时，物品不会被移除，这里只修改可升级的武器
     if self.inst.components.finiteuses then
         self.inst.components.finiteuses:SetOnFinished(function(inst)
             setItemEnable(self, false)
         end)
     end
+
+    --- 由于官方机制设定，护甲类型的物品耐久为0会被移除
+    --- 修改官方函数容易引发bug，所以这里采用耐久小于10%时自动卸下装备的机制，避免损坏
+    if self.inst.components.armor then
+        self.inst:ListenForEvent("percentusedchange", function(inst, data)
+            if data.percent <= 0.1 and inst.components.equippable then
+                if self.inst.ksfunitemowner then
+                    inst.components.equippable:Unequip(self.inst.ksfunitemowner)
+                end
+            end
+        end)
+    end
+
+
 end
 
 
