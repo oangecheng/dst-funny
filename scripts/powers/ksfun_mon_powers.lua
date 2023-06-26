71b95d723adcefc8bf2a45d47bcb1144e5a661ab
chengzhi <chengzhi@kuaishou.com>
2023-06-26 20:44:24 +0800

    添加文件

---
+++ scripts/powers/ksfun_mon_powers.lua
@@ -0,0 +1,187 @@
+local NAMES = KSFUN_TUNING.MONSTER_POWER_NAMES
+local monsterpowers = {}
+
+
+--- 计算属性等级上限，四舍五入
+local function calcPowerMax(defaultlv)
+    return math.floor((1 + KSFUN_TUNING.DIFFCULTY) * defaultlv + 0.5) 
+end
+
+
+--- 计算怪物特殊攻击命中概率
+local function canHit(defaultratio)
+    local r =  KSFUN_TUNING.DEBUG and 1 or (1 + KSFUN_TUNING.DIFFCULTY) * defaultratio
+    return math.random(100) < r * 100
+end
+
+
+
+--- 设置属性上限
+--- @param defaultlv 默认的上限
+--- @param max 部分属性需要有限制，超过上限会出现问题
+local function setPowerMaxLv(inst, defaultlv, max)
+    local lv =  calcPowerMax(defaultlv)
+    lv = max and math.min(lv, max) or lv
+    inst.components.ksfun_level:SetMax(lv)
+end
+
+
+
+
+
+
+------ 怪物额外减伤 ----------------------------------------------------------------------------------------
+local function updateAbsorbStatus(inst)
+    local health = inst.target.components.health  
+    local lv = inst.components.ksfun_level:GetLevel()
+    if health then
+        health.externalabsorbmodifiers:SetModifier("ksfun_power", lv * 0.01)
+    end
+end
+
+local absorb = {
+    power = {
+        onAttachFunc = function(inst, target, name)
+            -- 最多减伤30%，最高难度50%
+            setPowerMaxLv(inst, 30, 50)
+            updateAbsorbStatus(inst)
+        end
+    },
+    level = {
+        onLvChangeFunc = updateAbsorbStatus
+    }
+}
+
+
+
+
+
+
+
+------ 怪物冰爆属性 ----------------------------------------------------------------------------------------
+local FREEZABLE_TAGS = { "freezable" }
+local NO_TAGS = { "FX", "NOCLICK", "DECOR", "INLIMBO" }
+
+local function doIceExplosion(inst, area, coldness)
+    --- 不知道这个效果有没有
+    if inst.components.freezable == nil then
+        MakeMediumFreezableCharacter(inst, "body")
+    end
+    inst.components.freezable:SpawnShatterFX()
+    inst:RemoveComponent("freezable")
+
+    local x, y, z = inst.Transform:GetWorldPosition()
+    local ents = TheSim:FindEntities(x, y, z, area, FREEZABLE_TAGS, NO_TAGS)
+    for i, v in pairs(ents) do
+        if v.components.freezable ~= nil then
+            v.components.freezable:AddColdness(coldness)
+        end
+    end
+    inst.SoundEmitter:PlaySound("dontstarve/creatures/hound/icehound_explo")
+end
+
+--- 死亡时有20%概率造成范围冰冻，冰冻范围和效果受等级影响
+--- 冰冻范围 [2, 4]
+--- 冰冻效果 [1, 2]
+local function onDeath(inst)
+    local power = inst.components.ksfun_power_system:GetPower(NAMES.ICE_EXPLOSION)
+    local hit = canHit(0.2)
+    if hit and power and power.components.ksfun_power:IsEnable() then
+        local lv = power.components.ksfun_level:GetLevel()
+        local area = 2 + 2 * lv/10
+        local coldness = 1 + lv/10
+        doIceExplosion(inst, area, coldness)
+    end
+end
+
+
+local iceexplosion = {
+    power = {
+        onAttachFunc = function(inst, target, name)
+            if target.prefab ~= "icehound" then
+                target:ListenForEvent("death", onDeath)
+                setPowerMaxLv(inst, 10, 20)
+            end
+        end
+    },
+    level = {}
+}
+
+
+
+
+
+
+
+------ 怪物降智光环增强 ----------------------------------------------------------------------------------------
+local delta = TUNING.SANITYAURA_SMALL / 25
+
+local function updateSanityauraStatus(inst)
+    local power = inst.components.ksfun_power
+    local data  = power:GetData()
+    if data and inst.target then
+        if inst.target.components.sanityaura then
+            local lv = inst.components.ksfun_level:GetLevel()
+            inst.target.components.sanityaura.aura = data.aura - lv * delta
+        end
+    end
+end
+
+local sanityaura = {
+    power = {
+        onAttachFunc = function(inst, target, name)
+            if target.components.sanityaura == nil then
+                target:AddComponent("sanityaura")
+            end
+            inst.components.ksfun_power:SetData( {aura = target.components.sanityaura.aura} )
+            -- 最高不超过巨鹿
+            setPowerMaxLv(inst, 50, 100)
+            updateSanityauraStatus(inst)
+        end
+    },
+    level = {
+        onLvChangeFunc = updateSanityauraStatus
+    }
+}
+
+
+
+
+
+------ 怪物额外真实伤害 ----------------------------------------------------------------------------------------
+local function realdamageAttack(attacker, data)
+    local power = attacker.components.ksfun_power_system:GetPower(NAMES.REAL_DAMAGE)
+    -- 20% 的概率造成属性等级点的额外真实伤害，不计算护甲
+    local hit = canHit(0.2)
+    if hit and power and data.target then
+        local lv = power.components.ksfun_level:GetLevel()
+        local health = data.target.components.health
+        if health then
+            health:DoDelta(-lv, nil, nil, true, nil, true)
+        end
+    end
+end
+
+local realdamage = {
+    power = {
+        onAttachFunc = function(inst, target, name)
+            setPowerMaxLv(inst, 20, 50)
+            inst.target:ListenForEvent("onattackother", realdamageAttack)
+        end
+    },
+    level = {}
+}
+
+
+
+
+
+
+
+monsterpowers.absorb       = { data = absorb } 
+monsterpowers.iceexplosion = { data = iceexplosion }
+monsterpowers.sanityaura   = { data = sanityaura }
+monsterpowers.realdamage   = { data = realdamage }
+
+
+return monsterpowers
