b4b5999e45dfcc3e87d2d5bdbc6ef0b201269721
chengzhi <chengzhi@kuaishou.com>
2023-06-29 17:50:27 +0800

    卷轴倒计时名称

--- scripts/prefabs/ksfun_task_reel.lua
+++ scripts/prefabs/ksfun_task_reel.lua
@@ -56,7 +56,7 @@ local function fn()
 
     -- 任务卷轴2分钟之后自动移除
     inst:AddComponent("timer")
-    inst.components.timer:StartTimer("ksfun_task_reel", 120)
+    inst.components.timer:StartTimer("ksfun_task_reel_timer", 120)
     inst:ListenForEvent("timerdone", function(inst, data)
         inst:DoTaskInTime(0, inst:Remove())
     end)
