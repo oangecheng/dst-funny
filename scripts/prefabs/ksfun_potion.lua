a32762f8aea0a7fa74d74c86e1b3346d0ea1df1b
chengzhi <chengzhi@kuaishou.com>
2023-08-03 19:52:57 +0800

    添加物品魔法药水

---
+++ scripts/prefabs/ksfun_potion.lua
@@ -0,0 +1,76 @@
+
+
+local assets = {
+	Asset("ANIM" , "anim/ksfun_potion.zip"),
+    Asset("IMAGE", "images/ksfun_potion.tex"),
+    Asset("ATLAS", "images/ksfun_potion.xml"),
+}
+
+local NAMES = KSFUN_TUNING.PLAYER_POWER_NAMES
+
+
+local itemsdef = {
+    ["butter"]        = NAMES.HEALTH,
+    ["meat_dried"]    = NAMES.HUNGER,
+    ["nightmarefuel"] = NAMES.SANITY,
+    ["berries"]       = NAMES.PICK,
+    ["dragonfruit"]   = NAMES.FARM,
+}
+
+
+local function abletoaccepttest(inst, item, giver)
+    return inst.power == nil and table.containskey(itemsdef, item.prefab)
+end
+
+
+local function onitemgive(inst, giver, item)
+    inst.power = itemsdef[item.prefab]
+end
+
+
+
+local function fn()
+    local inst = CreateEntity()
+
+    inst.entity:AddTransform()
+    inst.entity:AddAnimState()
+    inst.entity:AddNetwork()
+
+    MakeInventoryPhysics(inst)
+
+
+    inst.AnimState:SetBank("ksfun_potion")
+    inst.AnimState:SetBuild("ksfun_potion")
+    inst.AnimState:PlayAnimation("idle")
+
+    inst:AddTag("ksfun")
+    inst.entity:SetPristine()
+
+    inst.displaynamefn = function(aaa)
+        return "魔法药水"
+    end
+
+    if not TheWorld.ismastersim then
+        return inst
+    end
+
+    inst:AddComponent("inspectable")
+    inst:AddComponent("trader")
+    inst.components.trader:SetAbleToAcceptTest(abletoaccepttest)
+    inst.components.trader.onaccept = onitemgive
+
+    inst:AddComponent("inventoryitem")
+    inst.components.inventoryitem.atlasname = "images/ksfun_potion.xml"
+
+    inst.OnLoad = function(inst, data)
+        inst.power = data.power or nil
+    end
+    inst.OnSave = function(inst, data)
+        data.power = inst.power or nil
+    end
+
+
+    return inst
+end
+
+return Prefab("ksfun_potion", fn, assets)
