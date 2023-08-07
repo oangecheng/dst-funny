79ce760e85af87bc3e234bb7decf4cb4d26bb890
chengzhi <chengzhi@kuaishou.com>
2023-08-07 16:12:11 +0800

    突破添加次数记录

--- scripts/components/ksfun_breakable.lua
+++ scripts/components/ksfun_breakable.lua
@@ -2,6 +2,7 @@
 local KSFUN_BREAKABLE = Class(function(self, inst)
     self.inst = inst
     self.enable = false
+    self.count = 0
 
     self.onBreakFunc = nil
 end)
@@ -17,12 +18,18 @@ function KSFUN_BREAKABLE:Enable()
 end
 
 
+function KSFUN_BREAKABLE:GetCount()
+    return self.count
+end
+
+
 --- 装备突破，能够提升最大等级上限
 function KSFUN_BREAKABLE:Break(doer, item)
     KsFunLog("break start", self.inst.prefab, item.prefab, self.enable)
     if self.enable and self.onBreakFunc then
         if self.onBreakFunc(self.inst, doer, item) then
             item:DoTaskInTime(0, item:Remove())
+            self.count = self.count + 1
             KsFunLog("break success", self.inst.prefab)
             return true
         end
@@ -34,12 +41,14 @@ end
 function KSFUN_BREAKABLE:OnSave()
     return {
         enable = self.enable,
+        count  = self.count,
     }
 end
 
 
 function KSFUN_BREAKABLE:OnLoad(data)
     self.enable = data.enable
+    self.count  = data.count or 0
 end
 
 
